b42bd95e7325cc5feecc574e7a7221d7ee1e6a56
Raphael Barbo <rsbarbo@gmail.com>
2016-12-19 14:43:31 -0700

    update test and view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,8 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path
     else
-      redirect_to login_path
+      flash.now[:danger] = "Email and/or Password is invalid, please try again."
+      render :new
     end
   end
 
--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -5,9 +5,27 @@ RSpec.describe "User Sign Up" do
     it "expects to see the option to sign up" do
       # As an unauthenticated user
       # When I visit the root of the application "/"
-      visit "/"
+      visit '/'
       expect(page).to have_content('Sign Up')
       # I should be redirected to a page which prompts me to "Log In or Sign Up".
+    end
+  end
+
+  describe "when user click sign up" do
+    it "gets redirect to the sign up page" do
+      visit '/'
+      click_link 'Sign Up'
+
+      expect(current_path).to eq(signup_path)
+    end
+
+    it "expects page to have form to sign up" do
+      visit signup_path
+
+      expect(page).to have_content("Please Enter Name:")
+      expect(page).to have_content("Please Enter Email:")
+      expect(page).to have_content("Please Enter Password:")
+      expect(page).to have_content("Please Confirm Password:")
     end
   end
 end
