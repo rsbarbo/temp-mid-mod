fb9f82aa8ee2c43ab64d13715a121083b24b26f0
Raphael Barbo <rsbarbo@gmail.com>
2016-12-19 15:26:55 -0700

    update sessions controller and view (new) and user ligon spec and factories

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -7,41 +7,35 @@ RSpec.describe "User Login" do
       # When I visit the root of the application "/"
       visit '/'
       # I should be redirected to a page which prompts me to "Log In or Sign Up".
-      expect(page).to have_content('Log in')
+      expect(page).to have_content('Login')
     end
   end
 
-  describe "when user click sign up" do
-    it "gets redirect to the sign up page" do
+  describe "when user click login up" do
+    it "gets redirect to the login page" do
       visit '/'
-      click_link 'Sign Up'
+      click_link 'Login'
 
-      expect(current_path).to eq(signup_path)
+      expect(current_path).to eq(login_path)
     end
 
-    it "expects page to have form to sign up" do
-      visit signup_path
+    it "expects page to have form to login" do
+      visit login_path
 
-      expect(page).to have_content("Please Enter Name:")
       expect(page).to have_content("Please Enter Email:")
       expect(page).to have_content("Please Enter Password:")
-      expect(page).to have_content("Please Confirm Password:")
     end
   end
 
   it "creates the saves the user" do
-    visit signup_path
+    visit login_path
 
-    user = {name: "Raphael",
-            email: "email@example.com",
-            password: "password"}
+    user = create(:user)
 
-    fill_in "user_name", with: user[:name]
-    fill_in "user_email", with: user[:email]
-    fill_in "user_password", with: user[:password]
-    fill_in "user_password_confirmation", with: user[:password]
+    fill_in "email", with: user.email
+    fill_in "password", with: user.password
 
-    click_button("Create Account")
+    click_button("Log In")
 
     expect(current_path).to eq("/")
   end
--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :user do
     name "Andrew Carmer"
     email "email@example.com"
-    password_digest "password"
+    password "password"
   end
 end
