6f7ffe73973ac46b916f97f24dc0599058cc6b9d
Raphael Barbo <rsbarbo@gmail.com>
2016-12-19 17:41:36 -0700

    update user can edit created links spec to cover edge cases

--- spec/features/user_can_edit_created_links_spec.rb
+++ spec/features/user_can_edit_created_links_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe "Create Link" do
-  describe "when user visit the root page" do
-    it "expects to see the options to create a link" do
+  describe "when user visit the edit page for a link" do
+    it "expects to be able to submit a new link as long as the link is valid" do
 
       user = create(:user)
       user_login(user)
@@ -25,6 +25,33 @@ RSpec.describe "Create Link" do
       click_on "Submit"
 
       expect(current_path).to eq("/")
+    end
+    it "expects not to be able to submit a new link when link is not valid" do
+
+      user = create(:user)
+      user_login(user)
+
+      link = create(:link)
+
+      visit "/links/#{link.id}/edit"
+
+      expect(current_path).to eq("/links/#{link.id}/edit")
+
+      expect(page.find_field('link_title').value).to eq( link.title )
+      expect(page.find_field('link_url').value).to eq( link.url )
+
+      fill_in "link_title", with: "New Title"
+      fill_in "link_url", with: "youtube"
+
+      expect(page.find_field('link_title').value).to eq( "New Title" )
+      expect(page.find_field('link_url').value).to eq( "youtube" )
+
+      click_on "Submit"
+
+      expect(current_path).to eq("/links/#{link.id}/edit")
+
+      expect(page.find_field('link_title').value).to eq( link.title )
+      expect(page.find_field('link_url').value).to eq( link.url )
     end
   end
 end
